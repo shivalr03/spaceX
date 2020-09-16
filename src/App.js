9b4e1f49d2f5919a68b6b1d48fb345060aeab50f
shiva.kartik <shiva.kartik@happay.in>
2020-09-16 20:14:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Details from  './components/details';
+import Details from './components/details';
 import axios from "axios";
 import Button from "terra-button";
 import Loader from 'react-loader-spinner'
@@ -18,77 +18,76 @@ class App extends React.Component {
       isSuccessfulLand: '',
       isFilterDataLoading: '',
       unfilteredData: null,
-      
+
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     // Initial Call
     axios.get("https://api.spacexdata.com/v3/launches?limit=100", {})
-    .then((response) => {
-        this.setState({shuttleData: response.data, unfilteredData: response.data, isDetailsLoading: false})
-    });
+      .then((response) => {
+        this.setState({ shuttleData: response.data, unfilteredData: response.data, isDetailsLoading: false })
+      });
   }
 
   handleDataFetch = () => {
-    console.log(this.state);
     //Launch Success Filter
-    if(this.state.selectedYear === '' && this.state.isSuccessfulLaunch && (this.state.isSuccessfulLand === false || this.state.isSuccessfulLand === '')){
+    if (this.state.selectedYear === '' && this.state.isSuccessfulLaunch && (this.state.isSuccessfulLand === false || this.state.isSuccessfulLand === '')) {
       this.setState({
         isFilterDataLoading: true
       });
       axios.get("https://api.spacexdata.com/v3/launches?limit=100&launch_success=true", {})
-      .then((response) => {
-          this.setState({shuttleData: response.data, isFilterDataLoading: false})
-      });
+        .then((response) => {
+          this.setState({ shuttleData: response.data, isFilterDataLoading: false })
+        });
     }
 
     //Launch & Land Filter
-    if(this.state.selectedYear === '' && this.state.isSuccessfulLaunch && this.state.isSuccessfulLand){
+    if (this.state.selectedYear === '' && this.state.isSuccessfulLaunch && this.state.isSuccessfulLand) {
       this.setState({
         isFilterDataLoading: true
       });
       axios.get("https://api.spacexdata.com/v3/launches?limit=100&launch_success=true&land_success=true", {})
-      .then((response) => {
-          this.setState({shuttleData: response.data, isFilterDataLoading: false})
-      });
+        .then((response) => {
+          this.setState({ shuttleData: response.data, isFilterDataLoading: false })
+        });
     }
 
     //All Filter
-    if(this.state.selectedYear !== '' && this.state.isSuccessfulLaunch && this.state.isSuccessfulLand){
+    if (this.state.selectedYear !== '' && this.state.isSuccessfulLaunch && this.state.isSuccessfulLand) {
       this.setState({
         isFilterDataLoading: true
       });
       axios.get(`https://api.spacexdata.com/v3/launches?limit=100&launch_success=true&land_success=true&launch_year=${this.state.selectedYear}`, {})
-      .then((response) => {
-          this.setState({shuttleData: response.data, isFilterDataLoading: false})
-      });
+        .then((response) => {
+          this.setState({ shuttleData: response.data, isFilterDataLoading: false })
+        });
     }
   }
 
   handleFilter = (filterNum, yearInfo, launchSuccess, landSuccess) => {
     //Checking which filter is selected at a particular time depending on the filter number
-    if(filterNum === 1){
+    if (filterNum === 1) {
       // Data Filter Altered
       this.setState({
         selectedYear: this.state.selectedYear === yearInfo ? '' : yearInfo
-      }, ()=> {
+      }, () => {
         this.handleDataFetch();
       });
     }
-    else if(filterNum === 2){
+    else if (filterNum === 2) {
       // Successful Launch Filter Altered
       this.setState({
         isSuccessfulLaunch: launchSuccess
-      }, ()=>{
+      }, () => {
         this.handleDataFetch();
       });
     }
-    else{
+    else {
       // Successful Land Filter Altered
       this.setState({
         isSuccessfulLand: landSuccess
-      }, ()=> {
+      }, () => {
         this.handleDataFetch();
       });
     }
@@ -98,21 +97,21 @@ class App extends React.Component {
 
   render() {
 
-    let yearData = this.state.unfilteredData && this.state.unfilteredData.map((data,index) => {
+    let yearData = this.state.unfilteredData && this.state.unfilteredData.map((data, index) => {
       return data.launch_year;
     });
     let individualYear = yearData && yearData.filter((a, b) => yearData.indexOf(a) === b);
     let renObjData = individualYear && individualYear.map((yearInfo, idx) => {
       // Handling Dynamic render of Year Options
-        return(
-          <Button
+      return (
+        <Button
           key={idx}
           text={yearInfo}
           variant="emphasis"
           className={this.state.selectedYear === yearInfo ? "year active" : "year"}
-          onClick={(event) => {this.handleFilter(1, yearInfo, this.state.isSuccessfulLaunch, this.state.isSuccessfulLand)}}
-          /> 
-        ); 
+          onClick={(event) => { this.handleFilter(1, yearInfo, this.state.isSuccessfulLaunch, this.state.isSuccessfulLand) }}
+        />
+      );
     });
 
     return (
@@ -120,10 +119,10 @@ class App extends React.Component {
         <header>
           <h1>SpaceX Launch Programs</h1>
         </header>
-          <main>
-            {/* Displaying Loader when fetching data during initial render */}
-            {this.state.isDetailsLoading ? <Loader type="Oval" className="centered" color="#00BFFF" height={60} width={60} />:
-             <article>
+        <main>
+          {/* Displaying Loader when fetching data during initial render */}
+          {this.state.isDetailsLoading ? <Loader type="Oval" className="centered" color="#00BFFF" height={60} width={60} /> :
+            <article>
               <div className="menu">
                 <div className="container">
                   <h3 className="filter">Filter</h3>
@@ -141,49 +140,49 @@ class App extends React.Component {
                       <Button
                         text="True"
                         variant="emphasis"
-                        className={this.state.isSuccessfulLaunch === ''? "year": this.state.isSuccessfulLaunch ? "year active" : "year"}
-                        onClick={(event) => {this.handleFilter(2, this.state.selectedYear, true, this.state.isSuccessfulLand)}}
-                        />
+                        className={this.state.isSuccessfulLaunch === '' ? "year" : this.state.isSuccessfulLaunch ? "year active" : "year"}
+                        onClick={(event) => { this.handleFilter(2, this.state.selectedYear, true, this.state.isSuccessfulLand) }}
+                      />
 
                       <Button
                         text="False"
                         variant="emphasis"
-                        className={this.state.isSuccessfulLaunch === ''? "year": this.state.isSuccessfulLaunch ? "year" : "year active"}
-                        onClick={(event) => {this.handleFilter(2, this.state.selectedYear, false, this.state.isSuccessfulLand)}}
-                        />
+                        className={this.state.isSuccessfulLaunch === '' ? "year" : this.state.isSuccessfulLaunch ? "year" : "year active"}
+                        onClick={(event) => { this.handleFilter(2, this.state.selectedYear, false, this.state.isSuccessfulLand) }}
+                      />
                     </div>
                   </div>
 
                   <div className="filterThree">
                     <h4 className="SuccessHead">SuccessFul Landing</h4>
                     <div className="years">
-                    <Button
+                      <Button
                         text="True"
                         variant="emphasis"
-                        className={this.state.isSuccessfulLand === ''? "year": this.state.isSuccessfulLand ? "year active" : "year"}
-                        onClick={(event) => {this.handleFilter(3, this.state.selectedYear, this.state.isSuccessfulLaunch, true)}}
-                        />
+                        className={this.state.isSuccessfulLand === '' ? "year" : this.state.isSuccessfulLand ? "year active" : "year"}
+                        onClick={(event) => { this.handleFilter(3, this.state.selectedYear, this.state.isSuccessfulLaunch, true) }}
+                      />
 
                       <Button
                         text="False"
                         variant="emphasis"
-                        className={this.state.isSuccessfulLand === ''? "year": this.state.isSuccessfulLand ? "year" : "year active"}
-                        onClick={(event) => {this.handleFilter(3, this.state.selectedYear, this.state.isSuccessfulLaunch, false)}}
-                        />
+                        className={this.state.isSuccessfulLand === '' ? "year" : this.state.isSuccessfulLand ? "year" : "year active"}
+                        onClick={(event) => { this.handleFilter(3, this.state.selectedYear, this.state.isSuccessfulLaunch, false) }}
+                      />
                     </div>
                   </div>
                 </div>
               </div>
 
               {/* Displaying Loader during Filter Data Fetch */}
-              {this.state.isFilterDataLoading? <Loader type="Oval" className="centered" color="#00BFFF" height={60} width={60} /> : 
-                <Details shuttleData={this.state.shuttleData} isFilterDataLoading={this.state.isFilterDataLoading} /> }
-            </article> }
-          </main>
+              {this.state.isFilterDataLoading ? <Loader type="Oval" className="centered" color="#00BFFF" height={60} width={60} /> :
+                <Details shuttleData={this.state.shuttleData} />}
+            </article>}
+        </main>
         <footer>
           <h4 className="content">Developed by <span>Shiva Karthik</span></h4>
         </footer>
-    </div>
+      </div>
     )
   }
 }
